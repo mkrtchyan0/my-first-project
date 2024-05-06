7d0ed15bb4b95e8c089ce27fcd77032b45f933ae
Vardan Mkrtchyan <vardanmkrtchyan171@gmail.com>
2024-05-06 13:55:14 -0800

    first commit

---
+++ MyWebApp.Models/StudentModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Models
+{
+    public class StudentModel
+    {
+        /// <summary>
+        /// the private field id
+        /// </summary>
+        private int id;
+
+        /// <summary>
+        /// The private string name
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// the private string sureName
+        /// </summary>
+        private string sureName;
+
+        /// <summary>
+        /// the private int age
+        /// </summary>
+        private int age;
+
+        /// <summary>
+        /// the private int points
+        /// </summary>
+        private int points;
+
+
+        /// <summary>
+        /// Property ID
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    id = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Property Name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!value.Contains("0") || !value.Contains("1") || !value.Contains("2") || !value.Contains("3") ||
+                    !value.Contains("4") || !value.Contains("5") || !value.Contains("6") || !value.Contains("7") ||
+                    !value.Contains("8") || !value.Contains("9") || !value.Contains("0"))
+                {
+                    name = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Property SureName
+        /// </summary>
+        public string SureName
+        {
+            get
+            {
+                return sureName;
+            }
+            set
+            {
+                if (!value.Contains("0") || !value.Contains("1") || !value.Contains("2") || !value.Contains("3") ||
+                    !value.Contains("4") || !value.Contains("5") || !value.Contains("6") || !value.Contains("7") ||
+                    !value.Contains("8") || !value.Contains("9") || !value.Contains("0"))
+                {
+                    sureName = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Property Age
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prperty Points
+        /// </summary>
+        public int Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    points = value;
+                }
+            }
+        }
+    }
+}
---
+++ MyWebApp/Controllers/StudentController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace MyWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        /// <summary>
+        /// Students List
+        /// </summary>
+        public List<StudentModel> Students = new List<StudentModel>();
+
+        /// <summary>
+        /// Count for Adding "id" for every student
+        /// </summary>
+        int count = 1;
+
+        /// <summary>
+        /// Filepath for student xml document
+        /// </summary>
+        string filePath = @"students.xml";
+
+        public StudentController()
+        {
+            if (!System.IO.File.Exists(filePath))
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+
+                XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+                xmlDocument.AppendChild(xmlDeclaration);
+
+                XmlElement root = xmlDocument.CreateElement("root");
+                xmlDocument.AppendChild(root);
+
+                xmlDocument.Save("students.xml");
+            }
+
+            DeSerializerStudentsFromXml();
+        }
+
+        /// <summary>
+        /// Showing the Students
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult List()
+        {
+            return View(Students);
+        }
+
+        /// <summary>
+        /// Create a Student, the id creating auto
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        public IActionResult Create(StudentModel student)
+        {
+            if (student.Id == null || student.Name == null || 
+                student.SureName == null || student.Age == null || student.Points == null)
+            {
+                return View();
+            }
+            student.Id = count;
+            count++;
+
+            Students.Add(student);
+
+            SerializerStudentsToXml();
+
+            return RedirectToAction("List");
+        }
+
+        /// <summary>
+        /// Editing the Student values
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        public IActionResult Edit(StudentModel student)
+        {
+            if (student.Id == null || student.Name == null ||
+                student.SureName == null || student.Age == null || student.Points == null)
+            {
+                return View();
+            }
+
+            foreach (StudentModel studentModel in Students)
+            {
+                if (student.Id == studentModel.Id)
+                {
+                    int indexOfStudent = Students.IndexOf(studentModel);
+
+                    Students[indexOfStudent].Name = student.Name;
+                    Students[indexOfStudent].SureName = student.SureName;
+                    Students[indexOfStudent].Age = student.Age;
+                    Students[indexOfStudent].Points = student.Points;
+
+                    SerializerStudentsToXml();
+
+                    return RedirectToAction("List");
+                }
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// Delete the Student
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        public IActionResult Delete(StudentModel student)
+        {
+            if (student.Id == null)
+            {
+                return View();
+            }
+
+            foreach (StudentModel studentModel in Students)
+            {
+                if (student.Id == studentModel.Id)
+                {
+                    int indexOfStudent = Students.IndexOf(studentModel);
+
+                    Students.RemoveAt(indexOfStudent);
+                    
+                    SerializerStudentsToXml();
+
+                    return RedirectToAction("List");
+                }
+            }
+
+            return View();
+        }
+
+        /// <summary>
+        /// Serialize the List of Students to XML File
+        /// </summary>
+        private void SerializerStudentsToXml()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<StudentModel>));
+
+                using (TextWriter textWriter = new StreamWriter(filePath))
+                {
+                    xmlSerializer.Serialize(textWriter, Students);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Found errors" + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deserialize XML file to LIst of Students
+        /// </summary>
+        private void DeSerializerStudentsFromXml()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<StudentModel>));
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    Students = (List<StudentModel>)xmlSerializer.Deserialize(fileStream);  
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
